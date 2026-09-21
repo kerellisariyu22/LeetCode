7c475d2923551592956dc2681a48746e132d21a4
Sariyu Kerelli <kerellisariyu22@gmail.com>
2026-09-21 15:00:17 +0530

    Time: 1387 ms (89.63%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,5 +1,14 @@
-select name
-from SalesPerson
-where Sales_id 
-not in(select Sales_id from Orders
- where com_id=(select com_id from Company where name ='RED'))
+#select name
+#from SalesPerson
+#where Sales_id 
+#not in(select Sales_id from Orders
+ #where com_id=(select com_id from Company where name ='RED'))
+
+
+select s.name 
+from salesperson as s left join orders as o 
+on s.sales_id = o.sales_id
+left join company as c on o.com_id = c.com_id
+group by s.name
+having sum(c.name = 'RED') = 0
+or sum(c.name = 'RED') is NULL ;
